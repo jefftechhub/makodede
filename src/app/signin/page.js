67f450563a5cd527c43708ea23723d012403b6bd
jefftechhub <jefftechhub@gmail.com>
2024-04-24 02:29:44 -0700

    deploy

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useFormStatus } from "react-dom";
--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,1 @@
-import { NextResponse } from "next/server";
-import { Session } from "../mongooseSchema";
-import { cookies } from "next/headers";
 
-export function middleware(req) {
-  const cookie = req.cookies.get("session");
-
-  console.log(cookie.value);
-
-  // const userId = cookies.session;
-}
-
-//See "Matching Paths" below to learn more
-export const config = {
-  matcher: "/dashboard",
-};
